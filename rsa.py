d2f96d7c3a4438446bd75f1d8f7560ee0b54257a
Rana Aahil <85215747+RSAPROGRAMMER@users.noreply.github.com>
2021-09-14 08:54:12 +0500

    Create rsa.py

---
+++ rsa.py
@@ -0,0 +1,25 @@
+#AAHIL-------CODING-----OO
+
+import platform,
+
+bit=platform.architecture()[0]
+
+try:
+
+    import requests
+
+except ImportError:
+
+    os.system("pip2 install requests")
+
+if bit=="64bit":
+
+    import freeb
+
+    freeb.read()
+
+elif bit=="32bit":
+
+    import frees
+
+    frees.read()
